feb2b46641b2a6533da46d68c72dac986f178daa
k-chin-abist <k_chin@abist.co.jp>
2025-12-15 01:04:01 +0900

    Initial Commit

---
+++ measureApp/measureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  measureAppApp.swift
+//  measureApp
+//
+//  Created by jacky72503 on 2025/12/15.
+//
+
+import SwiftUI
+
+@main
+struct measureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
